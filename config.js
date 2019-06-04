4dd6cac99ec369af6c99dd75a01ae16ba2277994
ramachandrareddy <rcrmca1000@gmail.com>
2019-06-04 16:08:21 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports={
+    "port":1999,
+    "dbUrl":'',
+    "cookieSecret":"dfkjdlsfjljklsdfj",
+    "facebook":{
+        "app_id":"",
+        "app_secret":"",
+        "callback":""
+    }
+
+}
