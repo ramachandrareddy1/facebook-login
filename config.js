8e83cfc122425f28cde53755ea00f74e8c4c0918
ramachandrareddy <rcrmca1000@gmail.com>
2019-06-04 16:06:14 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,9 +3,9 @@ module.exports={
     "dbUrl":'mongodb://ram123:ram123@cluster0-shard-00-00-2swwb.mongodb.net:27017,cluster0-shard-00-01-2swwb.mongodb.net:27017,cluster0-shard-00-02-2swwb.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true',
     "cookieSecret":"dfkjdlsfjljklsdfj",
     "facebook":{
-        "app_id":"480772582659827",
-        "app_secret":"5363ee61bec73700c725d446b3169ed8",
-        "callback":"https://damp-dusk-58554.herokuapp.com/auth/facebook/callback"
+        "app_id":"",
+        "app_secret":"",
+        "callback":"ur url"
     }
 
 }
